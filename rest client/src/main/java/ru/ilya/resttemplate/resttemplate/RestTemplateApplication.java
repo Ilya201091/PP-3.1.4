0d1d10e4ccbf8f57ee556a397308159abc61b81f
ilya <ZiK201091@ya.ru>
2021-09-22 08:21:43 +0700

    rest-client

---
+++ rest client/src/main/java/ru/ilya/resttemplate/resttemplate/RestTemplateApplication.java
@@ -0,0 +1,36 @@
+package ru.ilya.resttemplate.resttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
+import ru.ilya.resttemplate.resttemplate.entite.User;
+
+@SpringBootApplication
+public class RestTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplateApplication.class, args);
+
+        User userJames = new User(3L,"James","Brown",(byte)29);
+        User userThomas = new User(3L,"Thomas","Shelby",(byte)29);
+        Connection connection = new Connection(getRestTemplate(), getHttp());
+        connection.listUser();
+        connection.addUser(userJames);
+        connection.changeUser(userThomas);
+        connection.deleteUser(3);
+
+
+    }
+
+    @Bean
+    static public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+    @Bean
+    static public HttpHeaders getHttp() {
+        return new HttpHeaders();
+    }
+
+}
